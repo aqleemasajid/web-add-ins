123b76148761a0c555dffa56b740215beee372c9
aqleemasajid <67157834+aqleemasajid@users.noreply.github.com>
2023-08-24 04:33:29 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+Office.onReady(function() {
+    // Initialization code
+    console.log("Office.js is ready");
+    document.getElementById("content").innerHTML = "<h1>Hello World!</h1>";
+  });
+  
