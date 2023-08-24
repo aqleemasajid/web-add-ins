cf8513bb354ec12455f3472875d17b745dbbb310
aqleemasajid <67157834+aqleemasajid@users.noreply.github.com>
2023-08-24 12:24:48 +0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,26 @@
 Office.onReady(function() {
-    // Initialization code
-    console.log("Office.js is ready");
-    document.getElementById("content").innerHTML = "<h1>Hello World!</h1>";
-  });
+  // Initialization code
+  if (Office.context.requirements.isSetSupported('WordApi', '1.3')) {
+    // Get the current Word document
+    var document = Office.context.document;
+
+    // Load the properties you want to access
+    document.load('properties/title, properties/author, properties/lastModifiedBy, properties/lastModifiedTime', function(result) {
+      // Check if the load was successful
+      if (result.status === Office.AsyncResultStatus.Succeeded) {
+        // Display the document information in the add-in
+        var info = "Title: " + document.properties.title +
+                   "\nAuthor: " + document.properties.author +
+                   "\nLast Modified By: " + document.properties.lastModifiedBy +
+                   "\nLast Modified Time: " + document.properties.lastModifiedTime;
+        document.getElementById("content").innerHTML = "<pre>" + info + "</pre>";
+      } else {
+        console.log("Error loading document properties: " + result.error.message);
+      }
+    });
+  } else {
+    console.log("This API version is not supported.");
+  }
+});
+
   
